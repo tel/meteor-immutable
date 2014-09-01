226281709ba3f5552f766331457f4e2714333bc2
kraf <filip.krasnianski@gmail.com>
2014-09-01 20:47:30 +0200

    Fix client

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  summary: "Immutable Data Collections for Javascript",
-  version: "2.0.16",
+  summary: "Immutable Data Collections for Javascript.",
+  version: "2.0.17",
   git: "https://github.com/kraf/meteor-immutable.git"
 });
 
@@ -9,7 +9,8 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0.1');
-  api.addFiles('lib/require-immutable.js');
+  api.versionsFrom("METEOR@0.9.0.1");
+  api.addFiles("vendor/Immutable.js", "client");
+  api.addFiles("lib/require-immutable.js", "server");
   api.export("Immutable", ["server", "client"]);
 });
